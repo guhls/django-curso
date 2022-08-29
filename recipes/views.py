8ce0412a2b4dacebe3113d0cf3713b4a446bed38
Gustavo Silva <guhenriquels@gmail.com>
2022-08-29 15:46:39 -0300

    Created recipe page

--- recipes/views.py
+++ recipes/views.py
@@ -11,5 +11,8 @@ def home(request):
 def recipe(request, id):
     return render(
         request, "recipes/pages/recipe-view.html",
-        context={'recipe': get_fake()}
+        context={
+            'recipe': get_fake(),
+            'is_detail': True,
+            }
         )

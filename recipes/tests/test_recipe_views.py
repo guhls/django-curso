6036ef094307156eb2419c79f078bfc768a45d56
Gustavo Silva <guhenriquels@gmail.com>
2022-08-31 20:45:53 -0300

    Created unique tests file for tests in tests.py

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,20 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from . import views
-
-
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_ok(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/recipes/')
-
-    def test_category_url_ok(self):
-        category_url = reverse('recipes:category', args=(1, ))
-        self.assertEqual(category_url, '/recipes/category/1/')
-
-    def test_recipe_url_ok(self):
-        category_url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(category_url, '/recipes/1/')
+from recipes import views
 
 
 class RecipeViewsTest(TestCase):

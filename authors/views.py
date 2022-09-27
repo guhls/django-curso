6a9ac2d2bcbc95eed47c52a64176502789f6870e
Gustavo Silva <guhenriquels@gmail.com>
2022-09-27 10:15:06 -0300

    Created register with crsf_token

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,10 @@ from .forms import RegisterForm
 
 
 def register(request):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
 
     return render(request, 'authors/pages/register.html', context={
         'form': form

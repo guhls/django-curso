b7f941544289dce42bac2325c674ead398e1f076
Gustavo Silva <guhenriquels@gmail.com>
2022-09-27 13:42:54 -0300

    Modify create view to just receive POST request

--- authors/urls.py
+++ authors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('register/create/', views.create, name='create')
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
 def register(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    request.session['number'] = request.session.get('number', 0)
+    request.session['number'] += 1
+
+    form_data = request.session.get('form_data', None)
+    form = RegisterForm(form_data)
 
     return render(request, 'authors/pages/register.html', context={
         'form': form
     })
+
+
+def create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST
+    request.session['form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')

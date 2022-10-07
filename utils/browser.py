ee893b8b7d66e0e4def552d6040f266ca5788c58
Gustavo Silva <guhenriquels@gmail.com>
2022-10-07 14:04:07 -0300

    Added SELENIUM_HEADLESS constant from env

--- utils/browser.py
+++ utils/browser.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from time import sleep
 
@@ -9,14 +10,20 @@ ROOT_PATH = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = 'chromedriver'
 CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
 
+SELENIUM_HEADLESS = os.environ.get('SELENIUM_HEADLESS')
 
 # Definir: options -> service -> browser
+
+
 def make_chrome_browser(*options):
     chrome_options = webdriver.ChromeOptions()
 
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if SELENIUM_HEADLESS == '1':
+        chrome_options.add_argument('--headless')
 
     chrome_service = Service(executable_path=str(CHROMEDRIVER_PATH))
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
